9344584758533bae4df97a15b3f53a91ec88ef7a
Felpii <sanchezady794@gmail.com>
2024-09-11 11:32:01 -0500

    Add files via upload

---
+++ Lab/1.py
@@ -0,0 +1,11 @@
+# Usamos recursión con memoización para calcular números de Fibonacci
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    return memo[n]
+# Calcular el número de Fibonacci de 50
+resultado = fibonacci(50)
+print("Fibonacci de 50 es:", resultado)
